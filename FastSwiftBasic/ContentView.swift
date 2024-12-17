333b3d573224a14b51651cd42e1cd0d01e8f2f76
yerin <dpfls1022@naver.com>
2024-12-17 16:51:55 +0900

    Initial Commit

---
+++ FastSwiftBasic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FastSwiftBasic
+//
+//  Created by 박예린 on 12/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FastSwiftBasic/FastSwiftBasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastSwiftBasicApp.swift
+//  FastSwiftBasic
+//
+//  Created by 박예린 on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct FastSwiftBasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
