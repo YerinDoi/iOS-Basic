2fe6d1a1ed8a6d71123ccd351dc0569f530d2c28
yerin <dpfls1022@naver.com>
2024-12-17 16:59:52 +0900

    문법 활용해서 코딩해보기

--- FastSwiftBasic/ContentView.swift
+++ FastSwiftBasic/ContentView.swift
@@ -8,14 +8,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var names: [String] = ["박예린", "장복득", "박윤기", "박용빈"]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        List {
+            ForEach(names, id: \.self) { name in
+                if name == "박예린" {
+                    Text("나야 나! \(name)님 반갑습니다.")
+                } else {
+                    Text("\(name)님 반갑습니다.")
+                }
+            }
         }
-        .padding()
     }
 }
 
